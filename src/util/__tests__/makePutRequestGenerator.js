65f2e80be7480bb3e3cb53df7c0934b9252e9bfd
jonathanramirez4479 <106727963+jonathanramirez4479@users.noreply.github.com>
2022-07-27 02:18:51 +0000

    test case solution

---
+++ src/util/__tests__/makePutRequestGenerator.js
@@ -0,0 +1,23 @@
+const makePutRequestGenerator = require("../makePutRequestGenerator")
+
+describe("makePutRequest", () => {
+    // expect(1).toEqual(1)
+    test("it should append allRequests when run", () => {
+        const allRequests = []
+        const makePutRequest = makePutRequestGenerator(allRequests)
+        makePutRequest('/put', function (req, res) {
+            res.writeHead(200);
+            res.end("My put test");
+        })
+        expect(allRequests.length).toEqual(1)
+        console.log(allRequests.length)
+        console.log(allRequests[0].toString())
+        let req = {url : "/put", method : "PUT"}
+        let res = {writeHead : jest.fn(), end : jest.fn()}
+        allRequests[0](req, res)
+        expect(res.writeHead).toHaveBeenCalled()
+        req = {url : "/put", method : "POST"}
+        res = {writeHead : jest.fn(), end : jest.fn()}
+        allRequests[0](req, res)
+        expect(res.writeHead).not.toHaveBeenCalled()
+})})
