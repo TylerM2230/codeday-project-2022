f13d0c7f982d029639286291aaaf40612ba891ce
Tyler M <tymiranda08@gmail.com>
2022-08-01 15:03:39 -0800

    add POST data

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-<<<<<<< HEAD
 const onsed = require("./src/onsed");
-=======
-const serverFramework = require("./src/framework");
-const fs = require('fs');
->>>>>>> cc4c207fd20a887d8f4b712ff7d50fe2a44232ed
+const fs = require("fs");
 
 const app = onsed();
 const PORT = 8000;
 const HOST = "localhost";
 
-let inMemData = {"hello" : "world"};
+let inMemData = { hello: "world" };
 
 app.makeGetRequest("/", (req, res) => {
-<<<<<<< HEAD
-  res.writeHead(200);
-  res.end("hello world!");
-=======
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('./index.html', null, function(error, data) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./index.html", null, function (error, data) {
     if (error) {
       res.writeHead(404);
-      res.write('File not found!');
-    } 
-    else {
+      res.write("File not found!");
+    } else {
       res.write(data);
     }
     res.end();
   });
- 
->>>>>>> cc4c207fd20a887d8f4b712ff7d50fe2a44232ed
 });
 
 app.makeGetRequest("/data", (req, res) => {
   res.writeHead(200);
   res.end(JSON.stringify(inMemData));
-  console.log(inMemData)
+  console.log(inMemData);
 });
 
 app.makePostRequest("/data", (req, res) => {
@@ -45,11 +34,6 @@ app.makePostRequest("/data", (req, res) => {
   console.log(inMemData);
 });
 
-<<<<<<< HEAD
 app.listen(PORT, HOST, () => {
-=======
-
-app.listen(PORT, () => {
->>>>>>> cc4c207fd20a887d8f4b712ff7d50fe2a44232ed
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
