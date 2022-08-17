f54819e3a85821bf54c08f1ddec36ff03420f1fa
Cindy Park <ccindyy82021@gmail.com>
2022-08-17 11:54:20 +0900

    added content to home component

--- public-site/src/client/pages/Home.js
+++ public-site/src/client/pages/Home.js
@@ -3,10 +3,22 @@ const Home = () => {
   return (
     <div className="container">
       <h1 className="text-center" style={{ paddingTop: "30%" }}>
-        Home
+      <p>version 1.0</p>
+            {/* logo */}
+            <h2>FIRST MESSAGE/INTRO ABOUT OUR WEB FRAMEWORK</h2>
+            <p>lorem ipsum</p>
+            <div className="code-snippet">
+                <p>$ show install code</p>
+            </div>
       </h1>
     </div>
   );
 };
 
 export default Home;
+
+
+
+
+
+
