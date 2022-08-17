2b69a381578f044f7a3674262ae53f3b4c8eaafc
Shubham Naik <shubhamnaik@Shubhams-Mac-mini.local>
2022-08-16 20:26:17 -0700

    chore: allow custom port

--- public-site/src/server/index.js
+++ public-site/src/server/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require('path');
 
 const app = onsed();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const HOST = "localhost";
 
 let inMemData = {};
