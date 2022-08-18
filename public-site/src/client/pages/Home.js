2107c40183e6d15378bfcc2a57336f27b9a5fe49
Cindy Park <ccindyy82021@gmail.com>
2022-08-18 11:45:17 +0900

    in progress

--- public-site/src/client/pages/Home.js
+++ public-site/src/client/pages/Home.js
@@ -10,12 +10,13 @@ const Home = () => {
       {/* logo */}
       <h2 style={{color:"white"}}><i>a minimal, unopinionated <br/>web framework</i></h2>
       <br/>
-      <div className="code-snippet" >
+      <div class="row">
+      <div class="col-sm-code" >
       <pre><code>$ npm i onsed@1.0.0</code></pre>
       </div>
-
-      <div className="go-to-docs"><a href='./Docs'><FaAlignJustify /> Read Docs</a></div>
-
+      <div class="col-sm-docs"><a href='./Docs'><FaAlignJustify /> Read Docs</a>
+      </div>
+      </div>
       <p><u>Current: v1.0.0</u></p>
       
     </div>
