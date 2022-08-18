2c8b1b4272375fd9ede63acf6a48a4606ff2f44b
Tyler M <tymiranda08@gmail.com>
2022-08-17 23:05:39 -0800

    add homepage

--- public-site/src/client/pages/home/Home.js
+++ public-site/src/client/pages/home/Home.js
@@ -1,26 +1,54 @@
 import React from "react";
-import { FaRegCopy } from "react-icons/fa";
+import { FaRegCopy, FaDollarSign } from "react-icons/fa";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./Home.css";
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-center" style={{ paddingTop: "15%", color: "white" }}>
+      <h1
+        className="text-center mb-3 fw-semibold"
+        style={{ paddingTop: "15%", color: "black" }}
+      >
         onsed.js
       </h1>
-      <h3 className="text-center font-italic">
-        a minimalist, unopinionated <br></br> framework for Node.js
-      </h3>
-      <div className="row">
-        <div className="col">
-          <CopyToClipboard text={"npm i onsed@1.0.0"}>
-            <FaRegCopy />
-          </CopyToClipboard>
+      <p className="text-sm-center font-weight-bold text-light lead mb-4">
+        a minimalist, unopinionated <br></br>framework for Node.js
+      </p>
+      <div className="d-flex flex-column flex-lg-row align-items-md-stretch justify-content-md-center gap-3 mb-4">
+        <div className="d-flex align-items-center justify-content-center bg-white rounded">
+          <div className="d-inline-flex gap-1">
+            <FaDollarSign size={25} />
+            <div className="highlight">
+              <pre tabindex="0" className="chroma mb-0">
+                <code className="language-sh" data-lang="sh">
+                  <span className="line">
+                    <span className="cl" style={{ marginRight: ".5rem" }}>
+                      npm i onsed@1.0.0
+                    </span>
+                  </span>
+                </code>
+              </pre>
+            </div>
+          </div>
+          <div style={{ cursor: "pointer" }}>
+            <CopyToClipboard text={"npm i onsed@1.0.0"}>
+              <FaRegCopy size={25} />
+            </CopyToClipboard>
+          </div>
         </div>
-        <div className="col">
-          <button>Read Docs</button>
-        </div>
+        <NavLink
+          className="btn btn-dark btn-lg bd-btn-lg btn-bd-primary d-flex align-items-center justify-content-center fw-semibold"
+          to="/docs"
+          exact
+        >
+          Read Docs
+        </NavLink>
       </div>
-      <h5 className="text-center">Current: v1.0.0</h5>
+      <p className="text-center text-muted mb-0 fs-10">
+        <strong>v1.0.0</strong>
+      </p>
     </div>
   );
 };
